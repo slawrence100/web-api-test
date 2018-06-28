af58360eadd9244dd443736e62b8db1bed13c2eb
Sydney L <sll224@cornell.edu>
2018-06-28 10:24:11 -0400

    Modified slice error

--- helpers.js
+++ helpers.js
@@ -1,8 +1,7 @@
 const renderRawResponse = (res) => {
-    // Takes the first 10 words from res
-    let trimmedResponse = res.slice(0, 10);
+
     // Manipulates responseField to render the unformatted response
-    responseField.innerHTML = `<text>${JSON.stringify(trimmedResponse)}</text>`;
+    responseField.innerHTML = `<text>${JSON.stringify(res)}</text>`;
 }
 
 var randInt = function (low, high) {
