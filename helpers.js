74fe95eb95e94bb44530107573e4ca8d1669a1f7
Sydney L <sll224@cornell.edu>
2018-06-28 11:05:44 -0400

    Attempted JSON processing

--- helpers.js
+++ helpers.js
@@ -5,6 +5,12 @@ const renderRawResponse = (res) => {
     responseField.innerHTML = `<text>${JSON.stringify(res)}</text>`;
 }
 
+const renderResponse = (res) => {
+    var json_response = JSON.parse(res);
+    responseField.innerHTML = '<p>' + json_response.name + "</p>";
+}
+
+
 var randInt = function (low, high) {
     return Math.floor(Math.random() * (high - low + 1));
 }
--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const xhr = new XMLHttpRequest();
 var url = "https://pokeapi.co/api/v2/pokemon/";
-
+var json_response;
 const responseField = document.getElementById("response-field");
 
 xhr.responseType = 'json';
 xhr.onreadystatechange = () => {
     if (xhr.readyState = XMLHttpRequest.DONE) {
-        renderRawResponse(xhr.response);
+        renderResponse(xhr.response);
     }
 }
 
